ad1f834a7290dd69dad3e3a093960f2db52c0114
Batman <arendaovosej639@gmail.com>
2025-08-20 19:22:55 +0900

    commit

---
+++ data.js
@@ -0,0 +1,78 @@
+const projectsData = {
+    "AYUB": [
+        {
+            name: "О нас",
+            path: "AYUB/about/index.html",
+            image: "images/ayub_about.jpg"
+        },
+        {
+            name: "Главная страница",
+            path: "AYUB/homepage/index1.html",
+            image: "images/ayub_homepage.jpg"
+        },
+        {
+            name: "Магазин",
+            path: "AYUB/shop/index1.html",
+            image: "images/ayub_shop.jpg"
+        },
+        {
+            name: "Отдельный товар",
+            path: "AYUB/shop single/index1.html",
+            image: "images/ayub_shop_single.jpg"
+        }
+    ],
+    "UMED": [
+        {
+            name: "404",
+            path: "UMED/404/index1.html",
+            image: "images/umed_404.jpg"
+        },
+        {
+            name: "Отдельный блог",
+            path: "UMED/blog single/index1.html",
+            image: "images/umed_blog_single.jpg"
+        },
+        {
+            name: "Контакты",
+            path: "UMED/contact/index1.html",
+            image: "images/umed_contact.jpg"
+        },
+        {
+            name: "Лицензии",
+            path: "UMED/Licences/index1.html",
+            image: "images/umed_licences.jpg"
+        },
+        {
+            name: "Защита паролем",
+            path: "UMED/Password Protect/index.html",
+            image: "images/umed_password_protect.jpg"
+        }
+    ],
+    "ISMOIL": [
+        {
+            name: "Портфолио",
+            path: "ISMOIL/portfilio/index1.html",
+            image: "images/ismoil_portfolio.jpg"
+        },
+        {
+            name: "Отдельное портфолио",
+            path: "ISMOIL/portfilio single/index1.html",
+            image: "images/ismoil_portfolio_single.jpg"
+        },
+        {
+            name: "Услуги",
+            path: "ISMOIL/service/index1.html",
+            image: "images/ismoil_service.jpg"
+        },
+        {
+            name: "Отдельная услуга",
+            path: "ISMOIL/service single/index1.html",
+            image: "images/ismoil_service_single.jpg"
+        },
+        {
+            name: "Команда",
+            path: "ISMOIL/team/index.html",
+            image: "images/ismoil_team.jpg"
+        }
+    ]
+};
