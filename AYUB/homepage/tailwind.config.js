14dad622f40f97b5ebd59f9805768e1a350163fe
Batman <arendaovosej639@gmail.com>
2025-08-22 10:25:49 +0900

    commit

---
+++ AYUB/homepage/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./AYUB/about/**/*.{html,js}",
+    "./AYUB/homepage/**/*.{html,js}",
+    "./AYUB/shop/**/*.{html,js}",
+    "./AYUB/shop single/**/*.{html,js}",
+    "./UMED/404/**/*.{html,js}",
+    "./UMED/blog single/**/*.{html,js}",
+    "./UMED/contact/**/*.{html,js}",
+    "./UMED/Licences/**/*.{html,js}",
+    "./UMED/Password Protect/**/*.{html,js}",
+    "./ISMOIL/portfilio/**/*.{html,js}",
+    "./ISMOIL/portfilio single/**/*.{html,js}",
+    "./ISMOIL/service/**/*.{html,js}",
+    "./ISMOIL/service single/**/*.{html,js}",
+    "./ISMOIL/team/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
